a5a150088a3dc7f3f1c54cdfe8ea3c81a049cf95
tania <taniavieco@gmail.com>
2023-11-30 13:13:58 +0100

    ff

--- Curso23_24/Tema 2 BD/Practica_9/vistas/vista_conf_borrar.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "<p>Se dispone usted a borrar la Pelicula con Id: <strong>".$_POST["btnBorrar"]."</strong></p>";
-echo "<form action='index.php' method='post'>";
-echo "<input type='hidden' name='nombre_caratula' value='".$_POST["nombre_caratula"]."'>";
-echo "<p><button type='submit' name='btnContBorrar' value='".$_POST["btnBorrar"]."'>Continuar</button> ";
-echo "<button type='submit'>Atrás</button></p>";
-echo "</form>";
-?>
---
+++ ExamenPhp/index.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+function error_page($titulo,$body){
+    $page='<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>'.$titulo.'</title>
+    </head>
+    <body>'.$body.'</body>
+    </html>';
+    return $page;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Examen2 PHP</h1>
+    
+    
+</body>
+</html>
