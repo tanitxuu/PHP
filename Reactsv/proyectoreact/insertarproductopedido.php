73733a92c913d82b4f9f6d819fe2972b5525bd40
tania <taniavieco@gmail.com>
2024-05-08 12:54:19 +0200

    y

--- Reactsv/proyectoreact/insertarproductopedido.php
+++ Reactsv/proyectoreact/insertarproductopedido.php
@@ -4,22 +4,20 @@ header("Access-Control-Allow-Origin: *");
 $_POST = json_decode(file_get_contents("php://input"), true);
 require 'funcion.php';
 
-$id_pedido = $_POST['id_pedido'];  // Supongamos que este es un array
-$id_producto = $_POST['id_producto'];  // Supongamos que este es un array
-
-// Accede a los elementos individuales del array
-$id_pedido_primero = $id_pedido[0];  // Suponiendo que quieres el primer elemento del array
-$id_producto_primero = $id_producto[0];  // Suponiendo que quieres el primer elemento del array
+$id_pedido = $_POST['id_pedido']; 
+$id_producto = $_POST['id_producto'];  
 
 try {
-    $consulta = "INSERT INTO TANIA_pedidos_producto VALUES (?, ?)";
+    $consulta = "INSERT INTO  TANIA_pedidos_productos  (id_pedido, id_producto) VALUES (?, ?)";
     $sentencia = $conexion->prepare($consulta);
-    $sentencia->execute([$id_pedido_primero, $id_producto_primero]);
-    $respuesta["mensaje"] = "producto insertado con exito";
+    $sentencia->execute([$id_pedido, $id_producto]);
+
+
+    $respuesta["mensaje"] = "Producto insertado con éxito";
+   
 } catch (PDOException $e) {
-    $conexion = null;
-    $sentencia = null;
-    $respuesta["mensaje"] = "No se pudo insertar";
+    
+    $respuesta["mensaje"] = "No se pudo insertar el producto: " . $e->getMessage();
 }
 
 echo json_encode($respuesta);
