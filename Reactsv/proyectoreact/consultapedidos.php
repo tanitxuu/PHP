4388a65fd049e9db9d52ab317bead57c4c9d336f
Tania <taniavieco@gmail.com>
2024-05-17 16:01:48 +0200

    ee

--- Reactsv/proyectoreact/consultapedidos.php
+++ Reactsv/proyectoreact/consultapedidos.php
@@ -4,20 +4,23 @@ header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods:GET,POST, DELETE"); 
 header('Content-Type: application/json');
 
+$_POST = json_decode(file_get_contents("php://input"), true);
+$id_pedido=$_POST['id_pedido'];
 
-$id_pedido=$_GET['id_pedido'];
 require 'funcion.php';
 try {
-    $consulta = "select TANIA_pedidos.nombre, TANIA_pedidos.telefono, TANIA_pedidos.id_pedido, TANIA_pedidos.precio, TANIA_productos.nombre AS producto_nombre,TANIA_pedidos_productos.cantidad FROM TANIA_pedidos_productos INNER JOIN TANIA_pedidos ON TANIA_pedidos.id_pedido = TANIA_pedidos_productos.id_pedido INNER JOIN TANIA_productos ON TANIA_pedidos_productos.id_producto = TANIA_productos.id_producto where WHERE TANIA_pedidos.id_pedido = 21";
+    $consulta = "select TANIA_pedidos.nombre, TANIA_pedidos.telefono, TANIA_pedidos.id_pedido, TANIA_pedidos.precio, TANIA_productos.nombre AS producto_nombre,TANIA_pedidos_productos.cantidad FROM TANIA_pedidos_productos INNER JOIN TANIA_pedidos ON TANIA_pedidos.id_pedido = TANIA_pedidos_productos.id_pedido INNER JOIN TANIA_productos ON TANIA_pedidos_productos.id_producto = TANIA_productos.id_producto  WHERE TANIA_pedidos_productos.id_pedido = ?
+    ;";
     $sentencia = $conexion->prepare($consulta);
     $sentencia->execute([$id_pedido]);
 } catch (PDOException $e) {
     $conexion = null;
     $sentencia = null;
-    die(error_page("Primer Login", "<h1>Primer Login</h1><p>No he podido conectarse a la base de datos: " . $e->getMessage() . "</p>"));
+    die(error_page("Primer Login", "<h1>Primer Login</h1><p>No he podido conectarse a la base de datos: " . $e->getMessage() . "   Este es el id : $id_pedido</p>"));
 }
 
 $categorias = $sentencia->fetchAll(PDO::FETCH_ASSOC);
 
 echo json_encode($categorias);
+
 ?>
