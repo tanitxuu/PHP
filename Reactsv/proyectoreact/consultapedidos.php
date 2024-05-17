3b283b40d34adca4d33be51bd0ed449888659acb
tania <taniavieco@gmail.com>
2024-05-17 10:27:37 +0200

    t

--- Reactsv/proyectoreact/consultapedidos.php
+++ Reactsv/proyectoreact/consultapedidos.php
@@ -4,12 +4,13 @@ header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods:GET,POST, DELETE"); 
 header('Content-Type: application/json');
 
-$_POST = json_decode(file_get_contents("php://input"), true);
+
+$id_pedido=$_GET['id_pedido'];
 require 'funcion.php';
 try {
-    $consulta = "select TANIA_pedidos.nombre, TANIA_pedidos.telefono, TANIA_pedidos.id_pedido, TANIA_pedidos.precio, TANIA_productos.nombre AS producto_nombre,TANIA_pedidos_productos.cantidad FROM TANIA_pedidos_productos INNER JOIN TANIA_pedidos ON TANIA_pedidos.id_pedido = TANIA_pedidos_productos.id_pedido INNER JOIN TANIA_productos ON TANIA_pedidos_productos.id_producto = TANIA_productos.id_producto";
+    $consulta = "select TANIA_pedidos.nombre, TANIA_pedidos.telefono, TANIA_pedidos.id_pedido, TANIA_pedidos.precio, TANIA_productos.nombre AS producto_nombre,TANIA_pedidos_productos.cantidad FROM TANIA_pedidos_productos INNER JOIN TANIA_pedidos ON TANIA_pedidos.id_pedido = TANIA_pedidos_productos.id_pedido INNER JOIN TANIA_productos ON TANIA_pedidos_productos.id_producto = TANIA_productos.id_producto where WHERE TANIA_pedidos.id_pedido = 21";
     $sentencia = $conexion->prepare($consulta);
-    $sentencia->execute();
+    $sentencia->execute([$id_pedido]);
 } catch (PDOException $e) {
     $conexion = null;
     $sentencia = null;
