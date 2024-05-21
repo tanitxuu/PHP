a1fb1a8f4638c281f8dbebf948bd82d741ba1849
Tania <taniavieco@gmail.com>
2024-05-21 16:02:36 +0200

    termi

--- Reactsv/proyectoreact/consultapedidos.php
+++ Reactsv/proyectoreact/consultapedidos.php
@@ -9,13 +9,27 @@ $id_pedido=$_POST['id_pedido'];
 
 require 'funcion.php';
 try {
-    $consulta = "select TANIA_pedidos.nombre,TANIA_pedidos.estado, TANIA_pedidos.telefono, TANIA_pedidos.id_pedido, TANIA_pedidos.precio, TANIA_productos.nombre AS producto_nombre,TANIA_pedidos_productos.cantidad FROM TANIA_pedidos_productos INNER JOIN TANIA_pedidos ON TANIA_pedidos.id_pedido = TANIA_pedidos_productos.id_pedido INNER JOIN TANIA_productos ON TANIA_pedidos_productos.id_producto = TANIA_productos.id_producto  WHERE TANIA_pedidos_productos.id_pedido = ?
-    ;";
+    $consulta = "SELECT 
+    TANIA_pedidos.nombre,
+    TANIA_pedidos.estado,
+    TANIA_pedidos.telefono,
+    TANIA_pedidos.id_pedido,
+    TANIA_pedidos.precio,
+    TANIA_productos.nombre AS producto_nombre,
+    TANIA_pedidos_productos.cantidad 
+ FROM 
+    TANIA_pedidos_productos 
+ INNER JOIN 
+    TANIA_pedidos ON TANIA_pedidos.id_pedido = TANIA_pedidos_productos.id_pedido 
+ INNER JOIN 
+    TANIA_productos ON TANIA_pedidos_productos.id_producto = TANIA_productos.id_producto  
+ WHERE 
+    TANIA_pedidos_productos.id_pedido = ?";
+
     $sentencia = $conexion->prepare($consulta);
     $sentencia->execute([$id_pedido]);
 } catch (PDOException $e) {
-    $conexion = null;
-    $sentencia = null;
+ ;
     die(error_page("Primer Login", "<h1>Primer Login</h1><p>No he podido conectarse a la base de datos: " . $e->getMessage() . "   Este es el id : $id_pedido</p>"));
 }
 
--- Reactsv/proyectoreact/estadopedido.php
+++ Reactsv/proyectoreact/estadopedido.php
@@ -1,22 +1,25 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: GET, POST, DELETE"); 
+header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT");
 header('Content-Type: application/json');
 
-
+// Conexión a la base de datos
 require 'funcion.php';
-$_POST = json_decode(file_get_contents("php://input"), true);
-$id_pedido=$_POST['id_pedido'];
+
+// Capturar los datos de la solicitud PUT
+$input = json_decode(file_get_contents("php://input"), true);
+$id_pedido = $input['id_pedido'];
 
 try {
     $consulta = "UPDATE TANIA_pedidos SET estado='terminado' WHERE id_pedido=?";
     $sentencia = $conexion->prepare($consulta);
     $sentencia->execute([$id_pedido]);
-    $respuesta = array("mensaje" => "Pedido terminado con éxito");
+    $respuesta = array("id" => 'terminado');
 } catch (PDOException $e) {
     $respuesta = array("mensaje" => "No se pudo terminar el pedido: " . $e->getMessage());
 }
 
 echo json_encode($respuesta);
 ?>
+
