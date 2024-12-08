876194f39cc442f9c94928d9aae0c664953e022d
TaniaDev <taniavieco@gmail.com>
2024-12-08 18:03:11 +0100

    ultimo cambio

--- Reactsv/proyectoreact/estadopedido.php
+++ Reactsv/proyectoreact/estadopedido.php
@@ -9,19 +9,18 @@ require 'funcion.php';
 
 // Capturar los datos de la solicitud PUT
 $input = json_decode(file_get_contents("php://input"), true);
-$estado_pedido=$input['estado_pedido'];
-$id_pedido = $input['id_pedido'];
+$estado_pedido = $input['estado_pedido']; // Recibo 'estado_pedido'
+$id_pedido = $input['id_pedido']; // Recibo 'id_pedido'
 
 
 try {
     $consulta = "UPDATE pedidos SET estado=? WHERE id_pedido=?";
     $sentencia = $conexion->prepare($consulta);
-    $sentencia->execute([$estado_pedido,$id_pedido]);
-    $respuesta = array("estado" => 'estado cambiado con exito');
+    $sentencia->execute([$estado_pedido, $id_pedido]);
+    $respuesta = array("estado" => 'Estado cambiado con éxito');
 } catch (PDOException $e) {
-    $respuesta = array("mensaje" => "No se pudo terminar el pedido: " . $e->getMessage());
+    $respuesta = array("mensaje" => "No se pudo actualizar el estado del pedido: " . $e->getMessage());
 }
 
 echo json_encode($respuesta);
 ?>
-
